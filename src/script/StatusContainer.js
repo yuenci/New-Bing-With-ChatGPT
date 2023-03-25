6a3552e1212dc93e67167461d9824098127a1560
Innis <1575270674@qq.com>
2023-03-26 04:42:00 +0800

    feat: add tone options event

---
+++ src/script/StatusContainer.js
@@ -0,0 +1,10 @@
+let color = {
+    creative: "linear-gradient(90deg, #904887 10.79%, #8B257E 87.08%)",
+    balanced: "linear-gradient(90deg, #2870EA 10.79%, #1B4AEF 87.08%)",
+    precise: "linear-gradient(90deg, #006880 10.79%, #005366 87.08%)"
+}
+
+
+
+
+export { color }
--- src/script/tone.js
+++ src/script/tone.js
@@ -1,6 +1,8 @@
+import { color } from "./StatusContainer.js";
+
 export default function AddTone() {
-    let content =
-        `<div>Choose a conversation style</div>
+  let content =
+    `<div>Choose a conversation style</div>
       <div id="tone-con">
         <div class="tone-card">
           <div>More</div>
@@ -16,11 +18,35 @@ export default function AddTone() {
         </div>
       </div>`;
 
-    let tone = document.createElement('div');
-    tone.id = 'tone-container';
-    tone.innerHTML = content;
-    document.body.appendChild(tone);
-    addEvent(tone);
+  let tone = document.createElement('div');
+  tone.id = 'tone-container';
+  tone.innerHTML = content;
+  document.body.appendChild(tone);
+  addEvent(tone);
 }
 
-function addEvent(dom) { }
+let container, creativeBtn, balancedBtn, preciseBtn;
+
+function addEvent(dom) {
+  container = $(dom).find('#tone-con');
+  creativeBtn = $(container).find('.tone-card').eq(0);
+  balancedBtn = $(container).find('.tone-card').eq(1);
+  preciseBtn = $(container).find('.tone-card').eq(2);
+
+  clearBGC();
+
+  $(".tone-card").on('click', function () {
+    clearBGC();
+    $(this).css('background', color[$(this).find('div').eq(1).text().toLowerCase()]);
+    $(this).css('color', 'white');
+  });
+}
+
+function clearBGC() {
+  let btns = [creativeBtn, balancedBtn, preciseBtn];
+
+  for (let i = 0; i < btns.length; i++) {
+    $(btns[i]).css('background', 'white');
+    $(btns[i]).css('color', 'black');
+  }
+}
